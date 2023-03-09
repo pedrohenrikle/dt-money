a2b711935dff07f2e76e47abf1f56920d6152514
pedrohenrikle <pedro.klein.sl@gmail.com>
2023-03-09 19:38:04 -0300

    first commit

---
+++ src/pages/Transactions/components/SearchForm/index.tsx
@@ -0,0 +1,50 @@
+import { MagnifyingGlass } from 'phosphor-react'
+import { SearchFormContainer } from './styles'
+import * as zod from 'zod'
+import { zodResolver } from '@hookform/resolvers/zod'
+import { useForm } from 'react-hook-form'
+import { TransactionContext } from '../../../../contexts/TransactionsConntext'
+import { useContextSelector } from 'use-context-selector'
+
+const searchFormSchema = zod.object({
+  query: zod.string(),
+})
+
+type SearchFormInputs = zod.infer<typeof searchFormSchema>
+
+export function SearchForm() {
+  const fetchTransactions = useContextSelector(
+    TransactionContext,
+    (context) => {
+      return context.fetchTransactions
+    },
+  )
+
+  const {
+    register,
+    handleSubmit,
+    formState: { isSubmitting },
+  } = useForm<SearchFormInputs>({
+    resolver: zodResolver(searchFormSchema),
+  })
+
+  async function handleSearchTransactions(data: SearchFormInputs) {
+    await fetchTransactions(data.query)
+  }
+
+  return (
+    <SearchFormContainer onSubmit={handleSubmit(handleSearchTransactions)}>
+      <input
+        autoComplete="off"
+        type="text"
+        placeholder="Busque por transações"
+        {...register('query')}
+      />
+
+      <button type="submit" disabled={isSubmitting}>
+        <MagnifyingGlass size={20} />
+        Buscar
+      </button>
+    </SearchFormContainer>
+  )
+}
